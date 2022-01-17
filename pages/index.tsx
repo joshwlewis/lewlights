276fc10e4b2bda2d6573d92da484254c412945e3
Josh W Lewis <josh.w.lewis@gmail.com>
2022-01-16 18:54:52 -0600

    Enable prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
@@ -9,25 +7,26 @@ const Home: NextPage = () => {
       <div className="h-screen dots bg-blend-multiply">
         <Head>
           <title>LewLights - Lakeland TN Holiday Light Show</title>
-          <meta name="description" content="LewLights is an animated, synchronized Holiday Light Show in Lakeland, TN." />
+          <meta
+            name="description"
+            content="LewLights is an animated, synchronized Holiday Light Show in Lakeland, TN."
+          />
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <main className="container mx-auto">
-          <h1 className="text-2xl font-bold">
-            LewLights
-          </h1>
+          <h1 className="text-2xl font-bold">LewLights</h1>
           <svg width="24" height="24">
-            <path d="M 0 0 L 0 24 L 24 24 L 24 0 L 0 0
+            <path
+              d="M 0 0 L 0 24 L 24 24 L 24 0 L 0 0
                      M 6, 12
                      a 6 6 0 1 1  12 0
-                     a 6 6 0 1 1 -12 0 Z">
-
-          </path>
+                     a 6 6 0 1 1 -12 0 Z"
+            ></path>
           </svg>
         </main>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
